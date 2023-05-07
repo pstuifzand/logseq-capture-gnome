308080caf066e26ec64d2b681bf546d608b46451
Peter Stuifzand <peter@stuifzand.eu>
2023-05-07 22:20:34 +0200

    Add keybindings for escape and control return

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import sys
 import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
-from gi.repository import Gtk, Adw, Gdk
+from gi.repository import Gtk, Adw, Gdk, Gio
 import requests
 import os
-
+import datetime
+import json
 
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.token = os.environ['LOGSEQ_TOKEN']
+        with open(os.path.expanduser("~/.config/logseq-capture/config.json")) as f:
+            try:
+                config = json.load(f)
+                self.token = config['LOGSEQ_TOKEN']
+            except FileNotFoundError:
+                print("Please create ~/.config/logseq-capture/config.json")
+                sys.exit(1)
+            except KeyError:
+                print("Please set LOGSEQ_TOKEN")
+                sys.exit(1)
 
         self.box1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.set_child(self.box1)
@@ -20,6 +30,14 @@ class MainWindow(Gtk.ApplicationWindow):
         self.textarea.set_editable(True)
         self.textarea.set_cursor_visible(True)
         self.textarea.set_wrap_mode(Gtk.WrapMode.WORD)
+        # Set font size with css provider
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_data("""
+        textview {
+            font-size: 20px;
+        }
+        """, -1)
+        Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
         self.box1.set_margin_top(10)
         self.box1.set_margin_bottom(10)
@@ -32,10 +50,21 @@ class MainWindow(Gtk.ApplicationWindow):
         scrolledwindow.set_child(self.textarea)
         self.box1.append(scrolledwindow)
         button = Gtk.Button(label="Capture")
-        button.connect("clicked", self.on_button_clicked)
+        button.set_action_name("win.capture")
         self.box1.append(button)
 
-    def on_button_clicked(self, event):
+        action = Gio.SimpleAction.new("capture")
+        action.connect("activate", self.on_button_clicked)
+        self.add_action(action)
+
+        action = Gio.SimpleAction.new("close")
+        action.connect("activate", self.on_escape_pressed_event)
+        self.add_action(action)
+
+    def on_escape_pressed_event(self, action, parameter):
+        self.close()
+
+    def on_button_clicked(self, event, parameter):
         buffer = self.textarea.get_buffer()
         text = buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter(), True)
 
@@ -51,6 +80,9 @@ class MainWindow(Gtk.ApplicationWindow):
         """
         page = logseqCommand(self.token, "logseq.DB.datascriptQuery", [query, ":today"])
         uuid = page[0][0]['uuid']
+            
+        now = datetime.datetime.now()
+        text = f"**{now.strftime('%H:%M')}** #inbox {text}"
         logseqCommand(self.token, "logseq.Editor.insertBlock", [uuid, text])
 
         self.textarea.get_buffer().set_text("")
@@ -70,9 +102,12 @@ class MyApp(Adw.Application):
         self.connect('activate', self.on_activate)
 
     def on_activate(self, app):
+        app.set_accels_for_action("win.capture", ["<Control>Return"])
+        app.set_accels_for_action("win.close", ["Escape"])
+
         self.win = MainWindow(application=app)
         self.win.set_title("Logseq Capture")
-        self.win.set_default_size(400, 400)
+        self.win.set_default_size(800, 300)
         self.win.set_resizable(True)
         self.win.present()
 
